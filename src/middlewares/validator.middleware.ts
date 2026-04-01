d1a277e95622b0200fb6d12521894afb5164e3bd
Isaias Cireli Mezzari <isaiascirelimezzari18@gmail.com>
2026-04-01 17:23:52 -0300

    middleware validacao e erro em implementação

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,7 +1,7 @@
 //importação de tipos diretamente do express
 import type { Request, Response, NextFunction } from "express";
 
-export const validateCreateUser = (req: Request, res:Response, next:NextFunction)=>{
+export const validateAdd = (req: Request, res:Response, next:NextFunction)=>{
     const {email, password, username, name} = req.body;
 
     if(!email || !password || !username ||!name){
@@ -9,4 +9,42 @@ export const validateCreateUser = (req: Request, res:Response, next:NextFunction
             error: 'Dados obrigátorios não informados'
         })
     }
+
+    next();
+}
+
+export const validateGetById = (req: Request, res:Response, next:NextFunction)=>{
+    const {id} = req.params
+
+    if(!id){
+        return res.status(400).json({
+            error: 'Dado obrigátorio não informado'
+        })
+    }
+
+    next();
+}
+
+export const validateUpdate = (req: Request, res:Response, next:NextFunction)=>{
+    const {id} = req.params
+
+    if(!id){
+        return res.status(400).json({
+            error: 'Dado obrigátorio não informados'
+        })
+    }
+
+    next();
+}
+
+export const validateDelete = (req: Request, res:Response, next:NextFunction)=>{
+    const {id} = req.params
+
+    if(!id){
+        return res.status(400).json({
+            error: 'Dado obrigátorio não informado'
+        })
+    }
+
+    next();
 }
