de386c472932c06e96cabab994b5f4299a0ce9d0
Isaias Cireli Mezzari <isaiascirelimezzari18@gmail.com>
2026-04-06 17:07:39 -0300

    desativado a validação da variavel de ambiente de senha para fins de testes

--- src/database/dbMysql.ts
+++ src/database/dbMysql.ts
@@ -2,25 +2,25 @@
 import dotenv from 'dotenv';
 import mysql2 from 'mysql2/promise';
 import type { Pool } from 'mysql2/promise';
+import { AppError } from '../errors/appError.js';
 
 
 // tratando as variaveis de ambiente
 dotenv.config();
 
 if(!process.env.DB_HOST || 
-  !process.env.DB_USER || 
-  !process.env.DB_PASSWORD || 
+  !process.env.DB_USER ||
   !process.env.DB_DATABASE || 
   !process.env.DB_PORT
 ){
-    throw new Error('Variaveis de ambiente não definidas')
+    throw new AppError('Variaveis de ambiente não definidas', 500)
 }
 
 // abre pool de conexões para o banco de dados mySQL
 const pool:Pool = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD!,
     database: process.env.DB_DATABASE,
     port: Number(process.env.DB_PORT),
     waitForConnections: true,
