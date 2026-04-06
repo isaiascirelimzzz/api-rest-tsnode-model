8129460587c2d8f542b6822266c06760e25d5f3b
Isaias Cireli Mezzari <isaiascirelimezzari18@gmail.com>
2026-04-06 17:00:44 -0300

    condicionais e mensagens de disparo de erro do método de getById atualizados

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,31 +1,33 @@
 //importação de tipos diretamente do express
 import type { Request, Response, NextFunction } from "express";
 
-export const validateAdd = (req: Request, res:Response, next:NextFunction)=>{
-    const {email, password, username, name} = req.body;
 
-    if(!email || !password || !username ||!name){
+//creio nn precisar documentar, basta ler o codigo
+export const addUser = (req: Request, res:Response, next:NextFunction)=>{
+    const {email, password, username, nome} = req.body;
+
+    // if(!email || !password || !username ||!nome){
+    //     return res.status(400).json({
+    //         error: 'Dados obrigátorios não informados'
+    //     })
+    // }
+
+    next();
+}
+
+export const getUserById = (req: Request, res:Response, next:NextFunction)=>{
+    const id = Number(req.params.id)
+
+    if(!id || isNaN(id)){
         return res.status(400).json({
-            error: 'Dados obrigátorios não informados'
+            error: 'Dados obrigátorio não informado ou inválido'
         })
     }
 
     next();
 }
 
-export const validateGetById = (req: Request, res:Response, next:NextFunction)=>{
-    const {id} = req.params
-
-    if(!id){
-        return res.status(400).json({
-            error: 'Dado obrigátorio não informado'
-        })
-    }
-
-    next();
-}
-
-export const validateUpdate = (req: Request, res:Response, next:NextFunction)=>{
+export const updateUser = (req: Request, res:Response, next:NextFunction)=>{
     const {id} = req.params
 
     if(!id){
@@ -37,7 +39,7 @@ export const validateUpdate = (req: Request, res:Response, next:NextFunction)=>{
     next();
 }
 
-export const validateDelete = (req: Request, res:Response, next:NextFunction)=>{
+export const deleteUser = (req: Request, res:Response, next:NextFunction)=>{
     const {id} = req.params
 
     if(!id){
